82abdbe0ce7f2fe25cca2eb45c2410f0df120e4f
karamage <karamage.umauma@gmail.com>
2020-09-05 09:32:54 +0900

    tweak test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:my_levelup_story/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ProviderScope(child: MyLevelUpStoryApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
