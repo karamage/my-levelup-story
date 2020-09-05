8611e9fe16c7a900beba61f6d8e54bd54ebee81b
karamage <karamage.umauma@gmail.com>
2020-09-05 10:59:03 +0900

    home screen.

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:my_levelup_story/data/providers/counter_provider.dart';
+import 'package:my_levelup_story/ui/screens/home_screen.dart';
 
 // Note: CounterApp is a HookWidget, from flutter_hooks.
-class MyLevelUpStoryApp extends HookWidget {
+class MyLevelUpStoryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final state = useProvider(counterProvider.state);
-    final counter = useProvider(counterProvider);
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('MyLevelUpStory')),
-        body: Center(
-          child: Text(state.toString()),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed:() => counter.increment(),
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      ),
-    );
+    return HomeScreen();
   }
 }
---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:my_levelup_story/data/providers/counter_provider.dart';
+
+// Note: CounterApp is a HookWidget, from flutter_hooks.
+class HomeScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final state = useProvider(counterProvider.state);
+    final counter = useProvider(counterProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('MyLevelUpStory')),
+        body: Center(
+          child: Text(state.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:() => counter.increment(),
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
