c30e5d613343dbb1d3633738eb347c8302b6a2d8
karamage <karamage.umauma@gmail.com>
2020-09-11 16:12:29 +0900

    tweak datasource.

--- lib/data/providers/user_repository_provider.dart
+++ lib/data/providers/user_repository_provider.dart
@@ -1,6 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_levelup_story/data/providers/datasource_provider.dart';
 import 'package:my_levelup_story/data/repository/user_repository.dart';
 import 'package:my_levelup_story/data/repository/user_repository_impl.dart';
 
 final userRepositoryProvider = Provider<UserRepository>(
-        (ref) => UserRepositoryImpl());
+        (ref) => UserRepositoryImpl(ds: ref.read(datasourceRepositoryProvider)));
