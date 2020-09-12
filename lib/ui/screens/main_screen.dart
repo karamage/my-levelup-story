26f33f6006e9fc4213d800a4e9bccc1ce1f71daf
karamage <karamage.umauma@gmail.com>
2020-09-12 09:52:54 +0900

    splash screen.

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -5,16 +5,15 @@ import 'package:my_levelup_story/data/providers/app_state_provider.dart';
 import 'package:my_levelup_story/data/providers/auth_provider.dart';
 import 'package:my_levelup_story/ui/screens/home_screen.dart';
 import 'package:my_levelup_story/ui/screens/login_screen.dart';
+import 'package:my_levelup_story/ui/screens/splash_screen.dart';
 
 class MainScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final appState = useProvider(appStateProvider.state);
     final authState = useProvider(authProvider.state);
-    print("appState.isInitCompleted=${appState.isInitCompleted}");
     if (!appState.isInitCompleted) {
-      // TODO splash画面
-      //return
+      return SplashScreen();
     }
     return authState.isLogined ? HomeScreen(): LoginScreen();
   }
---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      //appBar: AppBar(title: Text('MyLevelUpStory app')),
+      body: Center(
+        child: Text("wait"),
+      ),
+    );
+  }
+}
