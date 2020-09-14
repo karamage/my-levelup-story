816236af65c6f48019a35958f1bfe69694337294
karamage <karamage.umauma@gmail.com>
2020-09-14 14:32:12 +0900

    tweak app state manage.

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_levelup_story/data/models/app_state_type.dart';
 import 'package:my_levelup_story/data/providers/app_state_provider.dart';
-import 'package:my_levelup_story/data/providers/auth_provider.dart';
 import 'package:my_levelup_story/ui/screens/home_screen.dart';
 import 'package:my_levelup_story/ui/screens/login_screen.dart';
 import 'package:my_levelup_story/ui/screens/splash_screen.dart';
