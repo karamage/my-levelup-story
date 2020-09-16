c2b1ae6bdb4d24d87010e3ebada3e782f338eb24
karamage <karamage.umauma@gmail.com>
2020-09-17 08:51:01 +0900

    init item.

--- lib/data/models/item.freezed.dart
+++ lib/data/models/item.freezed.dart
@@ -16,9 +16,32 @@ class _$ItemTearOff {
   const _$ItemTearOff();
 
 // ignore: unused_element
-  _Item call({String id}) {
+  _Item call(
+      {String id,
+      String title,
+      String body,
+      int likeCount,
+      int starCount,
+      int commentCount,
+      bool isPublish,
+      CategoryType category,
+      List<String> likedUserIds,
+      User user,
+      DateTime createdAt,
+      DateTime updatedAt}) {
     return _Item(
       id: id,
+      title: title,
+      body: body,
+      likeCount: likeCount,
+      starCount: starCount,
+      commentCount: commentCount,
+      isPublish: isPublish,
+      category: category,
+      likedUserIds: likedUserIds,
+      user: user,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
     );
   }
 }
@@ -28,6 +51,17 @@ const $Item = _$ItemTearOff();
 
 mixin _$Item {
   String get id;
+  String get title;
+  String get body;
+  int get likeCount;
+  int get starCount;
+  int get commentCount;
+  bool get isPublish;
+  CategoryType get category;
+  List<String> get likedUserIds;
+  User get user;
+  DateTime get createdAt;
+  DateTime get updatedAt;
 
   Map<String, dynamic> toJson();
   $ItemCopyWith<Item> get copyWith;
@@ -36,7 +70,21 @@ mixin _$Item {
 abstract class $ItemCopyWith<$Res> {
   factory $ItemCopyWith(Item value, $Res Function(Item) then) =
       _$ItemCopyWithImpl<$Res>;
-  $Res call({String id});
+  $Res call(
+      {String id,
+      String title,
+      String body,
+      int likeCount,
+      int starCount,
+      int commentCount,
+      bool isPublish,
+      CategoryType category,
+      List<String> likedUserIds,
+      User user,
+      DateTime createdAt,
+      DateTime updatedAt});
+
+  $UserCopyWith<$Res> get user;
 }
 
 class _$ItemCopyWithImpl<$Res> implements $ItemCopyWith<$Res> {
@@ -49,10 +97,48 @@ class _$ItemCopyWithImpl<$Res> implements $ItemCopyWith<$Res> {
   @override
   $Res call({
     Object id = freezed,
+    Object title = freezed,
+    Object body = freezed,
+    Object likeCount = freezed,
+    Object starCount = freezed,
+    Object commentCount = freezed,
+    Object isPublish = freezed,
+    Object category = freezed,
+    Object likedUserIds = freezed,
+    Object user = freezed,
+    Object createdAt = freezed,
+    Object updatedAt = freezed,
   }) {
     return _then(_value.copyWith(
       id: id == freezed ? _value.id : id as String,
+      title: title == freezed ? _value.title : title as String,
+      body: body == freezed ? _value.body : body as String,
+      likeCount: likeCount == freezed ? _value.likeCount : likeCount as int,
+      starCount: starCount == freezed ? _value.starCount : starCount as int,
+      commentCount:
+          commentCount == freezed ? _value.commentCount : commentCount as int,
+      isPublish: isPublish == freezed ? _value.isPublish : isPublish as bool,
+      category:
+          category == freezed ? _value.category : category as CategoryType,
+      likedUserIds: likedUserIds == freezed
+          ? _value.likedUserIds
+          : likedUserIds as List<String>,
+      user: user == freezed ? _value.user : user as User,
+      createdAt:
+          createdAt == freezed ? _value.createdAt : createdAt as DateTime,
+      updatedAt:
+          updatedAt == freezed ? _value.updatedAt : updatedAt as DateTime,
     ));
+  }
+
+  @override
+  $UserCopyWith<$Res> get user {
+    if (_value.user == null) {
+      return null;
+    }
+    return $UserCopyWith<$Res>(_value.user, (value) {
+      return _then(_value.copyWith(user: value));
+    });
   }
 }
 
@@ -60,7 +146,22 @@ abstract class _$ItemCopyWith<$Res> implements $ItemCopyWith<$Res> {
   factory _$ItemCopyWith(_Item value, $Res Function(_Item) then) =
       __$ItemCopyWithImpl<$Res>;
   @override
-  $Res call({String id});
+  $Res call(
+      {String id,
+      String title,
+      String body,
+      int likeCount,
+      int starCount,
+      int commentCount,
+      bool isPublish,
+      CategoryType category,
+      List<String> likedUserIds,
+      User user,
+      DateTime createdAt,
+      DateTime updatedAt});
+
+  @override
+  $UserCopyWith<$Res> get user;
 }
 
 class __$ItemCopyWithImpl<$Res> extends _$ItemCopyWithImpl<$Res>
@@ -74,26 +175,88 @@ class __$ItemCopyWithImpl<$Res> extends _$ItemCopyWithImpl<$Res>
   @override
   $Res call({
     Object id = freezed,
+    Object title = freezed,
+    Object body = freezed,
+    Object likeCount = freezed,
+    Object starCount = freezed,
+    Object commentCount = freezed,
+    Object isPublish = freezed,
+    Object category = freezed,
+    Object likedUserIds = freezed,
+    Object user = freezed,
+    Object createdAt = freezed,
+    Object updatedAt = freezed,
   }) {
     return _then(_Item(
       id: id == freezed ? _value.id : id as String,
+      title: title == freezed ? _value.title : title as String,
+      body: body == freezed ? _value.body : body as String,
+      likeCount: likeCount == freezed ? _value.likeCount : likeCount as int,
+      starCount: starCount == freezed ? _value.starCount : starCount as int,
+      commentCount:
+          commentCount == freezed ? _value.commentCount : commentCount as int,
+      isPublish: isPublish == freezed ? _value.isPublish : isPublish as bool,
+      category:
+          category == freezed ? _value.category : category as CategoryType,
+      likedUserIds: likedUserIds == freezed
+          ? _value.likedUserIds
+          : likedUserIds as List<String>,
+      user: user == freezed ? _value.user : user as User,
+      createdAt:
+          createdAt == freezed ? _value.createdAt : createdAt as DateTime,
+      updatedAt:
+          updatedAt == freezed ? _value.updatedAt : updatedAt as DateTime,
     ));
   }
 }
 
 @JsonSerializable()
 class _$_Item implements _Item {
-  const _$_Item({this.id});
+  const _$_Item(
+      {this.id,
+      this.title,
+      this.body,
+      this.likeCount,
+      this.starCount,
+      this.commentCount,
+      this.isPublish,
+      this.category,
+      this.likedUserIds,
+      this.user,
+      this.createdAt,
+      this.updatedAt});
 
   factory _$_Item.fromJson(Map<String, dynamic> json) =>
       _$_$_ItemFromJson(json);
 
   @override
   final String id;
+  @override
+  final String title;
+  @override
+  final String body;
+  @override
+  final int likeCount;
+  @override
+  final int starCount;
+  @override
+  final int commentCount;
+  @override
+  final bool isPublish;
+  @override
+  final CategoryType category;
+  @override
+  final List<String> likedUserIds;
+  @override
+  final User user;
+  @override
+  final DateTime createdAt;
+  @override
+  final DateTime updatedAt;
 
   @override
   String toString() {
-    return 'Item(id: $id)';
+    return 'Item(id: $id, title: $title, body: $body, likeCount: $likeCount, starCount: $starCount, commentCount: $commentCount, isPublish: $isPublish, category: $category, likedUserIds: $likedUserIds, user: $user, createdAt: $createdAt, updatedAt: $updatedAt)';
   }
 
   @override
@@ -101,12 +264,54 @@ class _$_Item implements _Item {
     return identical(this, other) ||
         (other is _Item &&
             (identical(other.id, id) ||
-                const DeepCollectionEquality().equals(other.id, id)));
+                const DeepCollectionEquality().equals(other.id, id)) &&
+            (identical(other.title, title) ||
+                const DeepCollectionEquality().equals(other.title, title)) &&
+            (identical(other.body, body) ||
+                const DeepCollectionEquality().equals(other.body, body)) &&
+            (identical(other.likeCount, likeCount) ||
+                const DeepCollectionEquality()
+                    .equals(other.likeCount, likeCount)) &&
+            (identical(other.starCount, starCount) ||
+                const DeepCollectionEquality()
+                    .equals(other.starCount, starCount)) &&
+            (identical(other.commentCount, commentCount) ||
+                const DeepCollectionEquality()
+                    .equals(other.commentCount, commentCount)) &&
+            (identical(other.isPublish, isPublish) ||
+                const DeepCollectionEquality()
+                    .equals(other.isPublish, isPublish)) &&
+            (identical(other.category, category) ||
+                const DeepCollectionEquality()
+                    .equals(other.category, category)) &&
+            (identical(other.likedUserIds, likedUserIds) ||
+                const DeepCollectionEquality()
+                    .equals(other.likedUserIds, likedUserIds)) &&
+            (identical(other.user, user) ||
+                const DeepCollectionEquality().equals(other.user, user)) &&
+            (identical(other.createdAt, createdAt) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdAt, createdAt)) &&
+            (identical(other.updatedAt, updatedAt) ||
+                const DeepCollectionEquality()
+                    .equals(other.updatedAt, updatedAt)));
   }
 
   @override
   int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(id);
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(id) ^
+      const DeepCollectionEquality().hash(title) ^
+      const DeepCollectionEquality().hash(body) ^
+      const DeepCollectionEquality().hash(likeCount) ^
+      const DeepCollectionEquality().hash(starCount) ^
+      const DeepCollectionEquality().hash(commentCount) ^
+      const DeepCollectionEquality().hash(isPublish) ^
+      const DeepCollectionEquality().hash(category) ^
+      const DeepCollectionEquality().hash(likedUserIds) ^
+      const DeepCollectionEquality().hash(user) ^
+      const DeepCollectionEquality().hash(createdAt) ^
+      const DeepCollectionEquality().hash(updatedAt);
 
   @override
   _$ItemCopyWith<_Item> get copyWith =>
@@ -119,12 +324,46 @@ class _$_Item implements _Item {
 }
 
 abstract class _Item implements Item {
-  const factory _Item({String id}) = _$_Item;
+  const factory _Item(
+      {String id,
+      String title,
+      String body,
+      int likeCount,
+      int starCount,
+      int commentCount,
+      bool isPublish,
+      CategoryType category,
+      List<String> likedUserIds,
+      User user,
+      DateTime createdAt,
+      DateTime updatedAt}) = _$_Item;
 
   factory _Item.fromJson(Map<String, dynamic> json) = _$_Item.fromJson;
 
   @override
   String get id;
+  @override
+  String get title;
+  @override
+  String get body;
+  @override
+  int get likeCount;
+  @override
+  int get starCount;
+  @override
+  int get commentCount;
+  @override
+  bool get isPublish;
+  @override
+  CategoryType get category;
+  @override
+  List<String> get likedUserIds;
+  @override
+  User get user;
+  @override
+  DateTime get createdAt;
+  @override
+  DateTime get updatedAt;
   @override
   _$ItemCopyWith<_Item> get copyWith;
 }
--- lib/data/models/item.g.dart
+++ lib/data/models/item.g.dart
@@ -9,9 +9,89 @@ part of 'item.dart';
 _$_Item _$_$_ItemFromJson(Map<String, dynamic> json) {
   return _$_Item(
     id: json['id'] as String,
+    title: json['title'] as String,
+    body: json['body'] as String,
+    likeCount: json['likeCount'] as int,
+    starCount: json['starCount'] as int,
+    commentCount: json['commentCount'] as int,
+    isPublish: json['isPublish'] as bool,
+    category: _$enumDecodeNullable(_$CategoryTypeEnumMap, json['category']),
+    likedUserIds:
+        (json['likedUserIds'] as List)?.map((e) => e as String)?.toList(),
+    user: json['user'] == null
+        ? null
+        : User.fromJson(json['user'] as Map<String, dynamic>),
+    createdAt: json['createdAt'] == null
+        ? null
+        : DateTime.parse(json['createdAt'] as String),
+    updatedAt: json['updatedAt'] == null
+        ? null
+        : DateTime.parse(json['updatedAt'] as String),
   );
 }
 
 Map<String, dynamic> _$_$_ItemToJson(_$_Item instance) => <String, dynamic>{
       'id': instance.id,
+      'title': instance.title,
+      'body': instance.body,
+      'likeCount': instance.likeCount,
+      'starCount': instance.starCount,
+      'commentCount': instance.commentCount,
+      'isPublish': instance.isPublish,
+      'category': _$CategoryTypeEnumMap[instance.category],
+      'likedUserIds': instance.likedUserIds,
+      'user': instance.user,
+      'createdAt': instance.createdAt?.toIso8601String(),
+      'updatedAt': instance.updatedAt?.toIso8601String(),
     };
+
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError('A value must be provided. Supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+
+  final value = enumValues.entries
+      .singleWhere((e) => e.value == source, orElse: () => null)
+      ?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
+}
+
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
+  if (source == null) {
+    return null;
+  }
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
+}
+
+const _$CategoryTypeEnumMap = {
+  CategoryType.none: 'none',
+  CategoryType.live: 'live',
+  CategoryType.health: 'health',
+  CategoryType.food: 'food',
+  CategoryType.home: 'home',
+  CategoryType.mind: 'mind',
+  CategoryType.human: 'human',
+  CategoryType.love: 'love',
+  CategoryType.money: 'money',
+  CategoryType.work: 'work',
+  CategoryType.beauty: 'beauty',
+  CategoryType.school: 'school',
+  CategoryType.future: 'future',
+  CategoryType.past: 'past',
+  CategoryType.incident: 'incident',
+  CategoryType.disaster: 'disaster',
+};
