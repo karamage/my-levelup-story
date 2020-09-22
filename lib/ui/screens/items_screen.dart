d4777981fe1a838071b2be11406e7782d04987af
karamage <karamage.umauma@gmail.com>
2020-09-23 08:48:18 +0900

    items screen for now.

---
+++ lib/ui/screens/items_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("ItemsScreen"),
+    );
+  }
+}
