5fabdf461101ff7d54c2087ab27ee69c5cfb8fa4
karamage <karamage.umauma@gmail.com>
2020-09-26 14:24:13 +0900

    implements DI datasource layer interface.

--- lib/data/providers/datasource_provider.dart
+++ lib/data/providers/datasource_provider.dart
@@ -1,5 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_levelup_story/data/datasource/firebase_datasource.dart';
+import 'package:my_levelup_story/data/datasource/remote_datasource.dart';
 
-final datasourceRepositoryProvider = Provider<FirebaseDatasource>(
+final datasourceRepositoryProvider = Provider<RemoteDatasource>(
         (ref) => FirebaseDatasource());
--- lib/ui/viewmodels/app_state_view_model.dart
+++ lib/ui/viewmodels/app_state_view_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:meta/meta.dart';
-import 'package:my_levelup_story/data/datasource/firebase_datasource.dart';
+import 'package:my_levelup_story/data/datasource/remote_datasource.dart';
 import 'package:my_levelup_story/data/models/app_state.dart';
 import 'package:my_levelup_story/data/models/app_state_type.dart';
 import 'package:state_notifier/state_notifier.dart';
 
 class AppStateViewModel extends StateNotifier<AppState> {
-  AppStateViewModel({@required FirebaseDatasource ds}) : _ds = ds, super(const AppState(stateType: AppStateType.init)) {}
-  final FirebaseDatasource _ds;
+  AppStateViewModel({@required RemoteDatasource ds}) : _ds = ds, super(const AppState(stateType: AppStateType.init)) {}
+  final RemoteDatasource _ds;
 
   Future<void> initApp() async {
     await _ds.initializeApp();
