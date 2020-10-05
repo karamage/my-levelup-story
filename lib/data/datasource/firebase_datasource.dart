a69d4f21b8b24ad9053135aff556f49be3bf0482
karamage <karamage.umauma@gmail.com>
2020-10-05 15:48:39 +0900

    get profile items query.

--- lib/data/datasource/firebase_datasource.dart
+++ lib/data/datasource/firebase_datasource.dart
@@ -151,6 +151,14 @@ class FirebaseDatasource implements RemoteDatasource {
     return _getPagingQuery(query, lastDate);
   }
 
+  Query _getProfileItemsQuery(String userId, DateTime lastDate) {
+    DocumentReference userRef = _getUserRef(userId);
+    Query query = _db.collection(ITEMS_PATH)
+        .where("userRef", isEqualTo: userRef)
+        .where("isPublic", isEqualTo: true)
+        .orderBy("createdAt", descending: true);
+    return _getPagingQuery(query, lastDate);
+  }
 
   Map<String, dynamic> _setIdParam(Map<String, dynamic> params) {
     params[ID_KEY] = _getNewFirestoreId();
