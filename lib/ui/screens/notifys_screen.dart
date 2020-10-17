378e00d44b94a248ddb875d3dba1d99ff2b4e5fe
karamage <karamage.umauma@gmail.com>
2020-10-17 10:32:02 +0900

    improve notify screen.

--- lib/ui/screens/notifys_screen.dart
+++ lib/ui/screens/notifys_screen.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_levelup_story/data/models/notification.dart' as n;
+import 'package:my_levelup_story/data/providers/notifications_provider.dart';
+import 'package:my_levelup_story/ui/widgets/easy_list_view.dart';
+import 'package:my_levelup_story/ui/widgets/loading_indicator.dart';
+import 'package:my_levelup_story/ui/widgets/notification_cell.dart';
 
 class NotifysScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("NotifysScreen"));
+    final state = useProvider(notificationsProvider.state);
+    final vm = useProvider(notificationsProvider);
+    useEffect((){
+      vm.reload();
+      return null;
+    }, []);
+    return EasyListView(
+      items: state.items,
+      onRefresh: vm.onRefresh,
+      onNext: vm.next,
+      buildCells: (items) =>
+          items.map((item) =>
+              NotificationCell(
+                item,
+                tapNotification,
+              )
+          ).toList(),
+      isLoading: state.isLoading,
+      loading: LoadingIndicator(),
+    );
+  }
+
+  Future<void> tapNotification(n.Notification item) {
+    //画面遷移する
   }
 }
