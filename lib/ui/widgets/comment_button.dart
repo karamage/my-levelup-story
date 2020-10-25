1b3c0b673a5046f858b22a71f7218cc90a9dbe78
karamage <karamage.umauma@gmail.com>
2020-10-25 09:42:24 +0900

    add comment button.

---
+++ lib/ui/widgets/comment_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:my_levelup_story/data/models/item.dart';
+
+
+class CommentButton extends StatelessWidget {
+  final Item item;
+  final Future<void> Function(Item item) tapComment;
+
+  CommentButton(this.item, this.tapComment);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: FlatButton.icon(
+        icon: Icon(
+          Icons.chat_bubble,
+          color: Colors.grey,
+          size: 24.0,
+          semanticLabel: 'Comment',
+        ),
+        label: Text("${item.commentCount > 0 ? item.commentCount:""}"),
+        color: Colors.white,
+        textColor: Theme.of(context).primaryColor,
+        onPressed: () {
+        },
+      ),
+    );
+  }
+}
